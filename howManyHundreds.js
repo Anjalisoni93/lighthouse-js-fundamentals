cabdfeeaa685a3ee1aa5516f58f2c0c967b89d86
Anjali Soni <anjalipatadiya@gmail.com>
2021-09-28 03:00:12 +0000

    howManyHundreds station assignment

---
+++ howManyHundreds.js
@@ -0,0 +1,13 @@
+let shipAmount;
+
+let howManyHundreds = function (amount) {
+  shipAmount = amount / 100;
+  shipAmount = Math.floor(shipAmount);
+  return shipAmount;
+}
+
+console.log(howManyHundreds(1000), "=?", 10);
+console.log(howManyHundreds(894), "=?", 8);
+console.log(howManyHundreds(520), "=?", 5);
+console.log(howManyHundreds(99), "=?", 0);
+console.log(howManyHundreds(0), "=?", 0);
